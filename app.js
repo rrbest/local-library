ff71bb102520a90212696d8154ed4a35006927e9
Ryan Olsen <ryano1110@gmail.com>
2017-05-07 17:09:59 -0700

    Added controllers

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index.js');
+var users = require('./routes/users.js');
+var catalog = require('./routes/catalog.js');
 
 var app = express();
 
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/catalog', catalog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
