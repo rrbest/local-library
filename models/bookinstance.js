94450fd6ca5c6d888862af636a83eb968f744e22
Ryan Olsen <ryano1110@gmail.com>
2017-05-08 21:07:07 -0700

    added date formatting

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,3 +1,4 @@
+var moment = require('moment');
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
@@ -7,6 +8,12 @@ var BookInstanceSchema = Schema({
 	imprint: {type: String, required: true},
 	status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
 	due_back: {type: Date, default: Date.now},
+});
+
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function(){
+	return moment(this.due_back).format('MMMM Do, YYYY');
 });
 
 // Virtual for bookinstance's URL
