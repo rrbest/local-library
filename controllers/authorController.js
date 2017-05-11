e0d0dfeb2f684e7a9d98eedce2373b72f348ac3e
Ryan Olsen <ryano1110@gmail.com>
2017-05-10 22:15:23 -0700

    added author detail page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,3 +1,6 @@
+var async = require('async');
+var Book = require('../models/book.js');
+
 var Author = require('../models/author');
 
 // Display list of all Authors
@@ -13,7 +16,22 @@ exports.author_list = function(req, res, next) {
 
 // Display detail page for a specific Author
 exports.author_detail = function(req, res, next) {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+    async.parallel({
+    	author: function(callback){
+    		Author.findById(req.params.id)
+    		.exec(callback);
+    	},
+
+    	authors_books: function(callback){
+    		Book.find({'author': req.params.id}, 'title summary')
+    		.exec(callback);
+    	},
+
+    }, function(err, results){
+    	if(err){return next(err);}
+    	//Successful, so render
+    	res.render('author_detail', {title: 'Author Detail', author: results.author, author_books: results.authors_books});
+    });
 };
 
 // Display Author create form on GET
