a69ce91669e8154b5810903483d388cd9f840380
Ryan Olsen <ryano1110@gmail.com>
2017-05-10 21:48:32 -0700

    added genre detail page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,3 +1,6 @@
+var Book = require('../models/book.js');
+var async = require('async');
+
 var Genre = require('../models/genre');
 
 // Display list of all Genre
@@ -13,7 +16,22 @@ exports.genre_list = function(req, res, next) {
 
 // Display detail page for a specific Genre
 exports.genre_detail = function(req, res, next) {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+    async.parallel({
+    	genre: function(callback){
+    		Genre.findById(req.params.id)
+    		.exec(callback);
+    	},
+
+    	genre_books: function(callback){
+    		Book.find({'genre': req.params.id})
+    		.exec(callback);
+    	},
+    }, function(err, results){
+    	if(err) {return next(err);}
+    	//Successful, so render
+    	res.render('genre_detail', {title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books});
+    });
+
 };
 
 // Display Genre create form on GET
