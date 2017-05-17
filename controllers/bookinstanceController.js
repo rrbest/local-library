c64ff82ba1c33add0dc1bcc18bdae29778d34fef
Ryan Olsen <ryano1110@gmail.com>
2017-05-16 21:37:01 -0700

    added bookinstance update

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,5 +1,6 @@
 var BookInstance = require('../models/bookinstance');
 var Book = require('../models/book');
+var async = require('async')
 
 // Display list of all BookInstances
 exports.bookinstance_list = function(req, res, next) {
@@ -103,10 +104,77 @@ exports.bookinstance_delete_post = function(req, res, next) {
 
 // Display BookInstance update form on GET
 exports.bookinstance_update_get = function(req, res, next) {
-    res.send('NOT IMPLEMENTED: BookInstance update GET');
+    req.sanitize('id').escape();
+    req.sanitize('id').trim();
+
+    // Get book, authors, and genres for form
+    async.parallel({
+        bookinstance: function(callback){
+            BookInstance.findById(req.params.id).populate('book').exec(callback)
+        },
+        books: function(callback){
+            Book.find(callback)
+        },
+
+    }, function(err, results){
+        if(err){return next(err);}
+        res.render('bookinstance_form', 
+            {title: 'Update BookInstance', 
+            book_list: results.books, 
+            selected_book: results.bookinstance.book_id,
+            bookinstance:results.BookInstanc
+            });
+    });
 };
 
 // Handle bookinstance update on POST
 exports.bookinstance_update_post = function(req, res, next) {
-    res.send('NOT IMPLEMENTED: BookInstance update POST');
+    req.sanitize('id').escape();
+    req.sanitize('id').trim();
+
+    req.checkBody('book', 'Book must be specified').notEmpty();
+    req.checkBody('imprint', 'Imprint must be specified').notEmpty();
+    req.checkBody('due_back', 'Invalid date').optional({checkFalsy: true}).isDate();
+
+    req.sanitize('book').escape();
+    req.sanitize('imprint').escape();
+    req.sanitize('status').escape();
+    req.sanitize('book').trim();
+    req.sanitize('imprint').trim();
+    req.sanitize('status').trim();
+    req.sanitize('due_back').toDate();
+
+    var bookinstance = new BookInstance(
+        {   book: req.body.book,
+            imprint: req.body.imprint,
+            status: req.body.status,
+            due_back: req.body.due_back,
+            _id: req.params.id
+        });
+
+    var errors = req.validationErrors();
+    if(errors){
+        Book.find({}, 'title')
+        .exec(function(err, books){
+            if(err){return next(err);}
+            //successful, so render
+            res.render('bookinstance_form', 
+                {title: 'Update BookInstance', 
+                book_list:books,
+                selected_books: bookinstance.book._id,
+                errors: errors,
+                bookinstance:BookInstance
+            });
+        });
+        return;
+    }
+    else{
+        // Data from form is valid
+        BookInstance.findByIdAndUpdate(req.params.id, bookinstance, {}, function(err, thebookinstance){
+            if(err){return next(err);}
+            //Successful - redirect to genre detail page.
+            res.redirect(thebookinstance.url);
+        });
+    }
+
 };
